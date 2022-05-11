1b7baea1a91ae5cf5714aac2cca553890a047278
Ruben Schmidmeister <4602612+bash@users.noreply.github.com>
2022-05-11 14:16:20 +0200

    Set up NuGet package

--- Broccolini/Editing/EditingExtensions.KeyValue.cs
+++ Broccolini/Editing/EditingExtensions.KeyValue.cs
@@ -7,6 +7,7 @@ namespace Broccolini.Editing;
 public static partial class EditingExtensions
 {
     /// <summary>Updates the value of a key-value node.</summary>
+    /// <param name="keyValueNode" />
     /// <param name="value">The value may contain anything except newlines. Quotes are automatically added as needed to preserve whitespace.</param>
     /// <exception cref="ArgumentException">Thrown when the updated node would result in something different when parsed back.</exception>
     [Pure]
