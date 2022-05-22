9eabe391e2101e59188ed531449fd1345f3b5e5c
Ruben Schmidmeister <4602612+bash@users.noreply.github.com>
2022-05-22 19:14:57 +0200

    Remove dead code

--- Broccolini/Editing/NewlineExtensions.cs
+++ Broccolini/Editing/NewlineExtensions.cs
@@ -8,23 +8,10 @@ internal static class NewLineExtensions
     public static IniDocument EnsureTrailingNewLine(this IniDocument document, Token.NewLine newLine)
         => document switch
         {
-            { Sections: { Count: >=1 } sections } => document with { Sections = document.Sections.ReplaceLast(n => EnsureTrailingNewLine(n, newLine)) },
-            { NodesOutsideSection: { Count: >= 1 } sections } => document with { NodesOutsideSection = document.NodesOutsideSection.ReplaceLast(n => EnsureTrailingNewLine(n, newLine)) },
+            { Sections: { Count: >=1 } sections } => document with { Sections = sections.ReplaceLast(n => EnsureTrailingNewLine(n, newLine)) },
+            { NodesOutsideSection: { Count: >= 1 } nodes } => document with { NodesOutsideSection = nodes.ReplaceLast(n => EnsureTrailingNewLine(n, newLine)) },
             _ => document,
         };
-
-    public static IniNode EnsureTrailingNewLine(this IniNode node, Token.NewLine newLine)
-        => node switch
-        {
-            SectionNode sectionNode => EnsureTrailingNewLine(sectionNode, newLine),
-            SectionChildNode sectionChildNode => EnsureTrailingNewLine(sectionChildNode, newLine),
-            _ => throw new InvalidOperationException("Unreachable"),
-        };
-
-    public static SectionChildNode EnsureTrailingNewLine(this SectionChildNode node, Token.NewLine newLine)
-        => node.NewLine is null
-            ? node with { NewLine = newLine }
-            : node;
 
     public static SectionNode EnsureTrailingNewLine(this SectionNode node, Token.NewLine newLine)
         => node switch
@@ -33,6 +20,11 @@ internal static class NewLineExtensions
             { Children.Count: 0, NewLine: null } => node with { NewLine = newLine },
             _ => node,
         };
+
+    private static SectionChildNode EnsureTrailingNewLine(this SectionChildNode node, Token.NewLine newLine)
+        => node.NewLine is null
+            ? node with { NewLine = newLine }
+            : node;
 
     private static IImmutableList<T> ReplaceLast<T>(this IImmutableList<T> list, Func<T, T> update)
         => list.SetItem(list.Count - 1, update(list[list.Count - 1]));
