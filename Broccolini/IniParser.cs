2be14648e0fe67697834a80afd3fd2cccf96d6fe
Ruben Schmidmeister <4602612+bash@users.noreply.github.com>
2023-04-20 19:44:52 +0200

    Fix cref

--- Broccolini/IniParser.cs
+++ Broccolini/IniParser.cs
@@ -1,6 +1,5 @@
 using System.Diagnostics.Contracts;
 using Broccolini.Parsing;
-using Broccolini.SemanticModel;
 using Broccolini.Syntax;
 using Broccolini.Tokenization;
 
@@ -9,7 +8,7 @@ namespace Broccolini;
 public static class IniParser
 {
     /// <summary><para>Parses an INI document to an AST. This function always succeeds even when invalid input is provided.</para>
-    /// <para>Use <see cref="IniDocumentExtensions.ToSemanticModel"/> to get a semantic view of the document
+    /// <para>Use <see cref="SemanticModel.IniDocumentExtensions.ToSemanticModel"/> to get a semantic view of the document
     /// or one of the methods on <see cref="Editing.EditingExtensions"/> to edit the document.</para></summary>
     [Pure]
     public static IniDocument Parse(string input)
