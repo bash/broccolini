2164772694563c22ad316bc92a5995d859f13662
Tau Gärtli <git@tau.garden>
2024-06-25 11:25:33 +0200

    Prevent accidental instantiations of Epsilon token

--- Broccolini/Syntax/IniToken.cs
+++ Broccolini/Syntax/IniToken.cs
@@ -111,6 +111,8 @@ public abstract record IniToken
 
     internal sealed record Epsilon : IniToken
     {
+        private Epsilon() { }
+
         public static readonly Epsilon Instance = new();
 
         public override string ToString() => string.Empty;
